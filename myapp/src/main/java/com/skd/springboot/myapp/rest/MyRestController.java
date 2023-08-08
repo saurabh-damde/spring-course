34fa712a470df34fee82fdc123ae48b0dab8528b
Saurabh Damde <saurabhdamde@gmail.com>
2023-08-08 14:41:57 +0530

    Server Configuration

--- myapp/src/main/java/com/skd/springboot/myapp/rest/MyRestController.java
+++ myapp/src/main/java/com/skd/springboot/myapp/rest/MyRestController.java
@@ -1,10 +1,23 @@
 package com.skd.springboot.myapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teamInfo")
+    public String teamInfo() {
+        return "Coach:" + coachName + ", Team: " + teamName;
+    }
+
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
