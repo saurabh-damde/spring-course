b79863fca6d5d7411b0aae9069e23ede912278d5
Saurabh Damde <saurabhdamde@gmail.com>
2023-08-14 11:04:44 +0530

    Bean Scopes

--- springcore/src/main/java/com/skd/springcore/common/BaseballCoach.java
+++ springcore/src/main/java/com/skd/springcore/common/BaseballCoach.java
@@ -1,8 +1,11 @@
 package com.skd.springcore.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BaseballCoach implements Coach {
 
     @Override
--- springcore/src/main/java/com/skd/springcore/rest/AppController.java
+++ springcore/src/main/java/com/skd/springcore/rest/AppController.java
@@ -9,16 +9,25 @@ import com.skd.springcore.common.Coach;
 
 @RestController
 public class AppController {
+
     private Coach coach;
+    private Coach anotherCoach;
 
     @Autowired
-    public AppController(@Qualifier("baseballCoach") Coach coach) {
+    public AppController(@Qualifier("baseballCoach") Coach coach,
+            @Qualifier("baseballCoach") Coach anotherCoach) {
         this.coach = coach;
+        this.anotherCoach = anotherCoach;
     }
 
     @GetMapping("/workout")
     public String getWorkout() {
         return coach.getWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comaparing Beans: coach & anotherCoach, " + (coach == anotherCoach);
     }
 
 }
