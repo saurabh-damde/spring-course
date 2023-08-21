285021a3ccaa8cd60251540ddef078e22659d59f
Saurabh Damde <saurabhdamde@gmail.com>
2023-08-21 10:09:33 +0530

    Initialize Springboot-Rest-CRUD Project

---
+++ springboot-rest-crud/src/main/java/com/skd/springbootrestcrud/SpringbootRestCrudApplication.java
@@ -0,0 +1,13 @@
+package com.skd.springbootrestcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestCrudApplication.class, args);
+	}
+
+}
---
+++ springboot-rest-crud/src/main/java/com/skd/springbootrestcrud/rest/TestRestController.java
@@ -0,0 +1,15 @@
+package com.skd.springbootrestcrud.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
