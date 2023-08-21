7cf2d76e8d252eb1eb9e08aca9cd9d3c199ca814
Saurabh Damde <saurabhdamde@gmail.com>
2023-08-21 12:07:16 +0530

    REST Exception Handling

--- springboot-rest-crud/src/main/java/com/skd/springbootrestcrud/rest/StudentRestController.java
+++ springboot-rest-crud/src/main/java/com/skd/springbootrestcrud/rest/StudentRestController.java
@@ -3,6 +3,9 @@ package com.skd.springbootrestcrud.rest;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,7 +35,28 @@ public class StudentRestController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if ((studentId >= students.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student ID: " + studentId + " not found!");
+        }
         return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        StudentErrorResponse response = new StudentErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage(),
+                System.currentTimeMillis());
+        return new ResponseEntity<StudentErrorResponse>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        StudentErrorResponse response = new StudentErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage(),
+                System.currentTimeMillis());
+        return new ResponseEntity<StudentErrorResponse>(response, HttpStatus.BAD_REQUEST);
     }
 
 }
