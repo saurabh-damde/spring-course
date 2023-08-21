c7b939d562347f74a877f9ced7a62d520c8a23a8
Saurabh Damde <saurabhdamde@gmail.com>
2023-08-21 11:37:43 +0530

    REST Path Variables

--- springboot-rest-crud/src/main/java/com/skd/springbootrestcrud/rest/StudentRestController.java
+++ springboot-rest-crud/src/main/java/com/skd/springbootrestcrud/rest/StudentRestController.java
@@ -4,21 +4,35 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skd.springbootrestcrud.entity.Student;
 
+import jakarta.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> students() {
-        List<Student> students = Arrays.asList(new Student("Saurabh", "Damde"),
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = Arrays.asList(new Student("Saurabh", "Damde"),
                 new Student("Sneha", "Shet"),
                 new Student("Shivani", "Kamalekar"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return students.get(studentId);
     }
 
 }
