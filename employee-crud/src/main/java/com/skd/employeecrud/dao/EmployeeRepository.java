c4c6a264e4547bf517c189254a13d3c09721569d
Saurabh Damde <saurabhdamde@gmail.com>
2023-08-23 10:14:41 +0530

    Spring Data REST Config

--- employee-crud/src/main/java/com/skd/employeecrud/dao/EmployeeRepository.java
+++ employee-crud/src/main/java/com/skd/employeecrud/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.skd.employeecrud.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.skd.employeecrud.entity.Employee;
 
+@RepositoryRestResource(path = "employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
