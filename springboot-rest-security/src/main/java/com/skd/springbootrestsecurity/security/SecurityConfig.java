ac880cab73e234400242e98598f19421c224fa6b
Saurabh Damde <saurabhdamde@gmail.com>
2023-08-28 15:38:11 +0530

    Add Database User Authentication

--- springboot-rest-security/src/main/java/com/skd/springbootrestsecurity/security/SecurityConfig.java
+++ springboot-rest-security/src/main/java/com/skd/springbootrestsecurity/security/SecurityConfig.java
@@ -1,37 +1,22 @@
 package com.skd.springbootrestsecurity.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}john")
-                .roles("EMPLOYEE")
-                .build();
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}mary")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}susan")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+    public UserDetailsManager detailsManager(DataSource source) {
+        return new JdbcUserDetailsManager(source);
     }
 
     @Bean
