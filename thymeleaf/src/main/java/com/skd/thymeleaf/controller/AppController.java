dbd25153485ed26464f642014f73a2a3f4b5023f
Saurabh Damde <saurabhdamde@gmail.com>
2023-08-29 12:35:41 +0530

    Initialize Spring MVC Thymeleaf Application

---
+++ thymeleaf/src/main/java/com/skd/thymeleaf/controller/AppController.java
@@ -0,0 +1,16 @@
+package com.skd.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("date", new java.util.Date());
+        return "helloworld";
+    }
+
+}
