736897792f62ff738d725ce86e3390024377cc6f
Saurabh Damde <saurabhdamde@gmail.com>
2023-09-05 15:32:49 +0530

    Validate number range

--- validation/src/main/java/com/skd/spring/mvc/validation/Customer.java
+++ validation/src/main/java/com/skd/spring/mvc/validation/Customer.java
@@ -1,5 +1,7 @@
 package com.skd.spring.mvc.validation;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,12 +13,17 @@ public class Customer {
 
     private String lastName;
 
+    @Min(value = 0, message = "must be greater than or equal to zero!")
+    @Max(value = 10, message = "must be less than or equal to ten!")
+    private int freePasses;
+
     public Customer() {
     }
 
-    public Customer(String firstName, String lastName) {
+    public Customer(String firstName, String lastName, int freePasses) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.freePasses = freePasses;
     }
 
     public String getFirstName() {
@@ -35,9 +42,17 @@ public class Customer {
         this.lastName = lastName;
     }
 
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
+
     @Override
     public String toString() {
-        return "Customer [firstName=" + firstName + ", lastName=" + lastName + "]";
+        return "Customer [firstName=" + firstName + ", lastName=" + lastName + ", freePasses=" + freePasses + "]";
     }
 
 }
