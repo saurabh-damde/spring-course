296a80a68e699662dc8e68d7108a4e90cd81f4bc
Saurabh Damde <saurabhdamde@gmail.com>
2023-09-05 15:16:03 +0530

    Implement InitBinder

--- validation/src/main/java/com/skd/spring/mvc/validation/CustomerController.java
+++ validation/src/main/java/com/skd/spring/mvc/validation/CustomerController.java
@@ -1,9 +1,12 @@
 package com.skd.spring.mvc.validation;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -11,6 +14,12 @@ import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor editor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, editor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
