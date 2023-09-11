099e030ec6be354a9b725e89936e04a3a03b6164
Saurabh Damde <saurabhdamde@gmail.com>
2023-09-11 15:43:29 +0530

    Implement Add Employee Module

--- thymleaf-employee-crud/src/main/java/com/skd/springboot/thymleafemployeecrud/controller/EmployeeController.java
+++ thymleaf-employee-crud/src/main/java/com/skd/springboot/thymleafemployeecrud/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.skd.springboot.thymleafemployeecrud.entity.Employee;
@@ -24,7 +26,20 @@ public class EmployeeController {
     public String listEmployees(Model model) {
         List<Employee> employees = service.findAll();
         model.addAttribute("employees", employees);
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/addEmployee")
+    public String addEmployee(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        service.save(employee);
+        return "redirect:/employees/list";
     }
 
 }
