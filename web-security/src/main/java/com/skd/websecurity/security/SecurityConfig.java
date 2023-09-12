daf2a74c542f918cbb6405a9e6e8618f81a27952
Saurabh Damde <saurabhdamde@gmail.com>
2023-09-12 12:54:15 +0530

    Add custom login page

--- web-security/src/main/java/com/skd/websecurity/security/SecurityConfig.java
+++ web-security/src/main/java/com/skd/websecurity/security/SecurityConfig.java
@@ -2,9 +2,11 @@ package com.skd.websecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -16,6 +18,13 @@ public class SecurityConfig {
         UserDetails susan = User.builder().username("susan").password("{noop}susan")
                 .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/loginPage").loginProcessingUrl("/authenticate").permitAll());
+        return http.build();
     }
 
 }
